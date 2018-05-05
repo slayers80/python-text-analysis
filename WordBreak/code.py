65c6c4c72890d258c62a4356987df753c439980c
slayers80 <freddie_slayers@hotmail.com>
2018-05-05 14:43:37 -0400

    Fruit

--- WordBreak/code.py
+++ WordBreak/code.py
@@ -33,3 +33,24 @@ def wordBreak(s, wordDict):
     visited = {}
 
     return dfs('', s)
+
+
+
+
+def wordBreak2(s, wordDict):
+    """
+    :type s: str
+    :type wordDict: List[str]
+    :rtype: bool
+    """
+    
+    
+    
+    d = [False]*len(s)
+    
+    for i in range(len(s)):
+        for w in wordDict:                
+            if w==s[i+1-len(w):i+1] and (d[i-len(w)] or i-len(w)==-1):
+                d[i] = True
+                       
+    return d[-1]
