79a7fc76c4abe24868125a8692c0d69e2dd5aa3d
slayers80 <freddie_slayers@hotmail.com>
2018-05-14 20:51:31 -0400

    maxSlidingWindow

--- maxSlidingWindow/code.py
+++ maxSlidingWindow/code.py
@@ -40,3 +40,23 @@ def maxSlidingWindow(nums, k):
             res.append(max_val)
             
     return res
+
+
+import collections
+def maxSlidingWindow2(nums, k):
+    """
+    :type nums: List[int]
+    :type k: int
+    :rtype: List[int]
+    """
+    d = collections.deque()
+    res = []
+    for i, num in enumerate(nums):
+        while d and nums[d[-1]]<num:
+            d.pop()
+        d.append(i)
+        if d[0]<i-k+1:
+            d.popleft()
+        if i>=k-1:
+            res.append(nums[d[0]])
+    return res
