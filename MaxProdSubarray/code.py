45f5241cbcd3ebdec1439b02758f1d8ad99245bd
slayers80 <freddie_slayers@hotmail.com>
2018-05-16 20:51:58 -0400

    MaxProdSubarray

---
+++ MaxProdSubarray/code.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 16 20:51:15 2018
+
+@author: lwang
+"""
+
+def maxProduct(nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
+    cprod1 = 1        
+    first_neg = False
+    max_seen = nums[0]-1
+    for num in nums:
+        if not num:
+            cprod1 = 1                                
+            first_neg = False
+            if max_seen<0:
+                max_seen = 0
+        else:                
+            cprod1 *= num    
+            if first_neg:  # 2nd negative number and after                    
+                cprod2 *= num                    
+                if cprod2>0 and cprod2>max_seen:
+                    max_seen = cprod2
+                elif cprod1>0 and cprod1>max_seen:
+                    max_seen = cprod1
+                    
+            else:      # first negative number
+                if cprod1 > max_seen:
+                    max_seen = cprod1
+                    
+                if num<0: 
+                    first_neg = True                    
+                    cprod2 = 1                    
+            
+    return max_seen
