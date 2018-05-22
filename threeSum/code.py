f6256a03ce88325d0cc7f23f61c9f7dc8233ff2b
slayers80 <freddie_slayers@hotmail.com>
2018-05-21 20:15:18 -0400

    threeSum

--- threeSum/code.py
+++ threeSum/code.py
@@ -34,3 +34,33 @@ def threeSum(nums):
                     if tuple(entry) not in res:
                         res.add(tuple(entry))
     return list(res)
+
+
+
+def threeSum2(self, nums):
+    """
+    :type nums: List[int]
+    :rtype: List[List[int]]
+    """ 
+    nums = sorted(nums)
+    res = [] 
+    
+    for i in range(0, len(nums)-2):
+        if i > 0 and nums[i] == nums[i-1]:
+            continue 
+        l, r = i+1, len(nums)-1 
+        while l < r:
+            currSum = nums[i] + nums[l] + nums[r]
+            if currSum < 0:
+                l += 1 
+            elif currSum > 0:
+                r -= 1 
+            else:
+                res.append([nums[i], nums[l], nums[r]])
+                while l < r and nums[l] == nums[l+1]:
+                    l += 1 
+                while l < r and nums[r] == nums[r-1]:
+                    r -= 1
+                l += 1 
+                r -= 1 
+    return res
