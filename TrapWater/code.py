3d31f8603d710728c1604ceb5b48ed6bca3b56e4
slayers80 <freddie_slayers@hotmail.com>
2018-05-27 11:11:37 -0400

    TrapWater

--- TrapWater/code.py
+++ TrapWater/code.py
@@ -28,3 +28,25 @@ def trap(height):
             vol += level-height[i] if level>height[i] else 0
             
     return vol
+
+
+def trap2(height):
+    """
+    :type height: List[int]
+    :rtype: int
+    """        
+    
+    if not height or len(height) < 3:
+        return 0
+    volume = 0
+    left, right = 0, len(height) - 1
+    l_max, r_max = height[left], height[right]
+    while left < right:
+        l_max, r_max = max(height[left], l_max), max(height[right], r_max)
+        if l_max <= r_max:
+            volume += l_max - height[left]
+            left += 1
+        else:
+            volume += r_max - height[right]
+            right -= 1
+    return volume
