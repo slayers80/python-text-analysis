3e506b9d4cada8752c772d3c4c521298b49d57ec
slayers80 <freddie_slayers@hotmail.com>
2018-05-28 20:53:53 -0400

    LongestIncreasingPath

--- LongestIncreasingPath/code.py
+++ LongestIncreasingPath/code.py
@@ -12,7 +12,7 @@ def longestIncreasingPath(matrix):
     """
     visited = {}        
     def dfs(i,j, m,n,matrix):
-        if (i,j) in visited:
+        if (i,j) in visited:            
             return visited[(i,j)]
         else:
             l = []
@@ -33,6 +33,7 @@ def longestIncreasingPath(matrix):
             else:
                 l.append(1)
             visited[(i,j)] = max(l)
+            #print (i,j), visited[(i,j)]
             return max(l)
     if matrix:
         m,n = len(matrix), len(matrix[0])
@@ -40,6 +41,28 @@ def longestIncreasingPath(matrix):
         for i in range(m):
             for j in range(n):
                 maxlen = max(maxlen, dfs(i,j,m,n,matrix))
+                #print maxlen
         return maxlen
     else:
         return 0
+    
+
+def longestIncreasingPath2(matrix):
+    """
+    :type matrix: List[List[int]]
+    :rtype: int
+    """
+    def dfs(i, j):
+        if not dp[i][j]:
+            val = matrix[i][j]
+            dp[i][j] = 1 + max(
+                dfs(i - 1, j) if i and val > matrix[i - 1][j] else 0,
+                dfs(i + 1, j) if i < M - 1 and val > matrix[i + 1][j] else 0,
+                dfs(i, j - 1) if j and val > matrix[i][j - 1] else 0,
+                dfs(i, j + 1) if j < N - 1 and val > matrix[i][j + 1] else 0)
+        return dp[i][j]
+
+    if not matrix or not matrix[0]: return 0
+    M, N = len(matrix), len(matrix[0])
+    dp = [[0] * N for i in range(M)]
+    return max(dfs(x, y) for x in range(M) for y in range(N))
