37b22b17dd0861f8a517806f1aa47385b5f5b256
slayers80 <freddie_slayers@hotmail.com>
2018-06-07 08:05:56 -0400

    LargestRectangleArea

--- LargestRectangleArea/code.py
+++ LargestRectangleArea/code.py
@@ -20,3 +20,21 @@ def largestRectangleArea(heights):
             areas.append(min(heights[j:i+1])*(i-j+1))
         maxarea = max(maxarea, max(areas))
     return maxarea
+
+
+def largestRectangleArea2(heights):
+    """
+    :type heights: List[int]
+    :rtype: int
+    """
+    heights.append(0)
+    stack = [-1]
+    ans = 0
+    for i in xrange(len(heights)):
+        while heights[i] < heights[stack[-1]]:
+            h = heights[stack.pop()]
+            w = i - stack[-1] - 1
+            ans = max(ans, h * w)
+        stack.append(i)
+    heights.pop()
+    return ans
