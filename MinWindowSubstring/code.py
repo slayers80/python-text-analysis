15a9362bc69f9bde449689607334d5bf8f6e9137
slayers80 <freddie_slayers@hotmail.com>
2018-06-10 10:10:49 -0400

    minWindowSubstring

--- MinWindowSubstring/code.py
+++ MinWindowSubstring/code.py
@@ -5,32 +5,40 @@ Created on Sat Jun 09 22:27:39 2018
 @author: lwang
 """
 
+
 def minWindow(s, t):
     """
     :type s: str
     :type t: str
     :rtype: str
-    """
-    targ = set(t)
+    """        
     
     minlen = 2147483649
     res= ""
+    
+    pool = list(t)
+    targ = set(t)
     if s:
         stack = []
         index = []        
-        for i in range(len(s)):
+        for i in range(len(s)):                
             if s[i] in targ:
                 stack.append(s[i])
-                index.append(i)
-                if set(stack)==targ:
-                    for j in range(len(stack)-1,-1,-1):
-                        if set(stack[j:])==targ:
-                            if minlen>index[-1]-index[j]:
-                                minlen = index[-1]-index[j]
-                                res = s[index[j]:index[-1]+1]
-                            while index and index[-1]-index[0]>=minlen:
-                                index.pop(0)
-                                stack.pop(0)
-                            break
+                index.append(i)           
+                j = len(stack)-1
+                while pool and j>=0:
+                    if stack[j] in pool:
+                        pool.remove(stack[j])
+                    j -= 1                            
+                if not pool:
+                    if minlen>index[-1]-index[j+1]+1:
+                        minlen = index[-1]-index[j+1]+1
+                        res = s[index[j+1]:index[-1]+1]
+                    while index and index[-1]-index[0]+1>=minlen:
+                        index.pop(0)
+                        stack.pop(0)
+                        
+                pool = list(t)                    
                             
-    return res 
+    return res             
+                
