386a803c2298597d3b7e027a70f7ebfcc3a72a34
slayers80 <freddie_slayers@hotmail.com>
2018-06-18 07:42:44 -0400

    WordSearch2

--- WordSearch2/code.py
+++ WordSearch2/code.py
@@ -63,3 +63,30 @@ def findWords(board, words):
                 dfs(i,j,m,n,list(remainingwords),parents,branch,res)                
                 
     return list(res)
+
+
+def findWords2(board, words):
+    
+    def dfs(board, i, j, trie, partial):
+        if '#' in trie:
+            res.add(partial)
+        if not (0 <= i < len(board)) or not (0 <= j < len(board[0])) or board[i][j] not in trie:
+            return
+        temp = board[i][j]
+        board[i][j] = '@'
+        [dfs(board, i+dx, j+dy, trie[temp], partial+temp) for dx, dy in directions]
+        board[i][j] = temp
+        
+    trie, res = dict(), set()
+    directions = ((0, 1), (0, -1), (1, 0), (-1, 0))
+    for word in words:
+        t = trie
+        for c in word:
+            if c not in t:
+                t[c] = dict()
+            t = t[c]
+        t['#'] = '#'
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            dfs(board, i, j, trie, '')
+    return res
